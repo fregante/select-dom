bfa49bd82fc0a368a0650fab06ef10a23689ed9f
Federico Brigante <bfred-it@users.noreply.github.com>
2016-06-03 12:19:15 +0700

    Add: select.all(selector, parentsArray)

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-module.exports = one;
-module.exports.all = all;
+'use strict';
 
-function one (selector, parent) {
-  if (!parent) parent = document;
-  return parent.querySelector(selector);
+function getElementsArray (elements) {
+	if (!elements) {
+		return [];
+	}
+	// some elements are array-like (<form> has a .length) so they need to be stopped here
+	if (elements instanceof HTMLElement || elements === document) {
+		return [elements];
+	}
+	if (elements.forEach) {
+		return elements;
+	}
+	if (Array.from) {
+		return Array.from(elements);
+	}
+	return Array.prototype.slice.call(elements);
 }
 
-function all (selector, parent) {
-  if (!parent) parent = document;
-  return parent.querySelectorAll(selector);
-}
+module.exports = function (selector, parent) {
+	return (parent || document).querySelector(selector);
+};
+
+module.exports.all = function (selector, parent) {
+	var current, i, l, ii, ll;
+	if (!parent) {
+		return getElementsArray(document.querySelectorAll(selector));
+	}
+	parent = getElementsArray(parent || document);
+	var all = [];
+	for (i = 0, l = parent.length; i < l; i++) {
+		current = parent[i].querySelectorAll(selector);
+		for (ii = 0, ll = current.length; ii < ll; ii++) {
+			if (all.indexOf(current[ii]) === -1) {
+				all.push(current[ii]);
+			}
+		}
+	}
+	return all;
+};
