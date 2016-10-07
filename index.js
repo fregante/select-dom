aa21f6e10c1d9611a5c87e079e030d914a897583
Federico Brigante <bfred-it@users.noreply.github.com>
2016-10-07 04:55:59 -0700

    Simplify code for array of parents

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 'use strict';
 
-function getElementsArray (elements) {
-	if (!elements) {
-		return [];
-	}
-	// some elements are array-like (<form> has a .length) so they need to be stopped here
-	if (elements instanceof HTMLElement || elements === document) {
-		return [elements];
-	}
-	if (Array.isArray(elements)) {
-		return elements;
-	}
-	if (Array.from) {
-		return Array.from(elements);
-	}
-	return Array.prototype.slice.call(elements);
-}
-
 module.exports = function (selector, parent) {
 	return (parent || document).querySelector(selector);
 };
 
 module.exports.all = function (selector, parent) {
-	var current, i, l, ii, ll;
+	// select.all('selector')
 	if (!parent) {
-		return getElementsArray(document.querySelectorAll(selector));
+		return Array.apply(document.querySelectorAll(selector));
 	}
-	parent = getElementsArray(parent || document);
+	
+	// select.all('selector', singleElementOrDocument)
+	if (typeof parent.querySelectorAll === 'function') {
+		parent = [parent];
+	}
+	
 	var all = [];
+	var current, i, l, ii, ll;
 	for (i = 0, l = parent.length; i < l; i++) {
 		current = parent[i].querySelectorAll(selector);
 		for (ii = 0, ll = current.length; ii < ll; ii++) {
-			if (all.indexOf(current[ii]) === -1) {
+			if (all.indexOf(current[ii]) < 0) {
 				all.push(current[ii]);
 			}
 		}
