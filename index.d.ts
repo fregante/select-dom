e99eeeb3aeac5470588cc450659145778591d24a
Federico Brigante <github@bfred.it>
2019-02-02 23:46:57 +0800

    Add support for NodeList and DocumentFragment

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,42 @@
 // https://github.com/Microsoft/TypeScript/blob/9d3707d671592d030386956c9ce39e539b8d0972/src/lib/dom.generated.d.ts#L10581
 
+type baseElements = Element | Element[] | NodeList | DocumentFragment;
+
 export interface SelectDom {
-	<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: Element): HTMLElementTagNameMap[K] | null;
-	<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: Element): SVGElementTagNameMap[K] | null;
-	<E extends Element = Element>(selectors: string, parent?: Element): E | null;
-	exists<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: Element): boolean;
-	exists<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: Element): boolean;
-	exists<E extends Element = Element>(selectors: string, parent?: Element): boolean;
-	all<K extends keyof HTMLElementTagNameMap>(selectors: K, parents?: Element|Element[]): HTMLElementTagNameMap[K][];
-	all<K extends keyof SVGElementTagNameMap>(selectors: K, parents?: Element|Element[]): SVGElementTagNameMap[K][];
-	all<E extends Element = Element>(selectors: string, parents?: Element|Element[]): E[];
+	<K extends keyof HTMLElementTagNameMap>(
+		selectors: K,
+		baseElement?: Element
+	): HTMLElementTagNameMap[K] | null;
+	<K extends keyof SVGElementTagNameMap>(
+		selectors: K,
+		baseElement?: Element
+	): SVGElementTagNameMap[K] | null;
+	<E extends Element = Element>(
+		selectors: string,
+		baseElement?: Element
+	): E | null;
+	exists<K extends keyof HTMLElementTagNameMap>(
+		selectors: K,
+		baseElement?: Element
+	): boolean;
+	exists<K extends keyof SVGElementTagNameMap>(
+		selectors: K,
+		baseElement?: Element
+	): boolean;
+	exists<E extends Element = Element>(
+		selectors: string,
+		baseElement?: Element
+	): boolean;
+	all<K extends keyof HTMLElementTagNameMap>(
+		selectors: K,
+		baseElements?: baseElements
+	): HTMLElementTagNameMap[K][];
+	all<K extends keyof SVGElementTagNameMap>(
+		selectors: K,
+		baseElements?: baseElements
+	): SVGElementTagNameMap[K][];
+	all<E extends Element = Element>(
+		selectors: string,
+		baseElements?: baseElements
+	): E[];
 }
