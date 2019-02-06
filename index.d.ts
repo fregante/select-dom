0bc46e815dca7e824afb4e80f87b90a584a102f4
Federico Brigante <github@bfred.it>
2019-02-06 09:47:35 +0800

    Add support for DocumentFragment on `select()`

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,32 @@
 // https://github.com/Microsoft/TypeScript/blob/9d3707d671592d030386956c9ce39e539b8d0972/src/lib/dom.generated.d.ts#L10581
 
+type BaseElement = Element | DocumentFragment;
 type BaseElements = Element | Element[] | NodeList | DocumentFragment;
 
 export interface SelectDom {
 	<K extends keyof HTMLElementTagNameMap>(
 		selectors: K,
-		baseElement?: Element
+		baseElement?: BaseElement
 	): HTMLElementTagNameMap[K] | null;
 	<K extends keyof SVGElementTagNameMap>(
 		selectors: K,
-		baseElement?: Element
+		baseElement?: BaseElement
 	): SVGElementTagNameMap[K] | null;
 	<E extends Element = Element>(
 		selectors: string,
-		baseElement?: Element
+		baseElement?: BaseElement
 	): E | null;
 	exists<K extends keyof HTMLElementTagNameMap>(
 		selectors: K,
-		baseElement?: Element
+		baseElement?: BaseElement
 	): boolean;
 	exists<K extends keyof SVGElementTagNameMap>(
 		selectors: K,
-		baseElement?: Element
+		baseElement?: BaseElement
 	): boolean;
 	exists<E extends Element = Element>(
 		selectors: string,
-		baseElement?: Element
+		baseElement?: BaseElement
 	): boolean;
 	all<K extends keyof HTMLElementTagNameMap>(
 		selectors: K,
